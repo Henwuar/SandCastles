bfeefa79509e5deae899b52a47d372a88805dc83
Henry Pullan <henrypullan@gmail.com>
2017-01-21 09:43:27 +0000

    added water shader

---
+++ Castles In the Sand/Assets/Scripts/Water.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    private Material material;
+    private float timer = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        material = GetComponent<Renderer>().material;	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.deltaTime;
+        material.SetFloat("_Timer", timer);
+	}
+}
